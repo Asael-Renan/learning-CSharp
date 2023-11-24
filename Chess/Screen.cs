0e8f8e64315c6bbd36df561c937ce984abcc634d
Asael Renan <asaelrenan5@gmail.com>
2023-11-24 19:13:00 -0300

    improving the implementation of the print board method

--- Chess/Screen.cs
+++ Chess/Screen.cs
@@ -8,17 +8,35 @@ namespace Chess
         {
             for (int i = 0; i < board.Rows; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < board.Columns; j++)
                 {
                     Piece piece = board.Piece(i, j);
                     if (piece != null)
                     {
-                        Console.Write(piece + " ");
-                    } else {
+                        Screen.PrintPiece(piece);
+                    }
+                    else
+                    {
                         Console.Write("- ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.Write("  a b c d e f g h");
+        }
+
+        public static void PrintPiece(Piece piece)
+        {
+            if (piece.Color == Color.White)
+            {
+                Console.Write(piece + " ");
+            }
+            if (piece.Color == Color.Black) {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(piece + " ");
+                Console.ForegroundColor = aux;
             }
         }
     }
